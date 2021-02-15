419633ea909bb5e42812f334e6c8fa2a8d79c7b4
Alaa Naeim <alaanaeim.iti@gmail.com>
2021-02-15 22:53:36 +0200

    updates

---
+++ npm-hello/index.js
@@ -0,0 +1,7 @@
+module.exports.hello = function () {
+  console.log("Hello In Nodejs");
+};
+
+module.exports.add = function (num1, num2) {
+  return num1 - num2;
+};
--- test2/index.js
+++ test2/index.js
@@ -1,4 +1,8 @@
-var _ = require("underscore");
+// var _ = require("underscore");
 
-var res = _.contains([1, 2, 3], 2);
-console.log(res);
+// var res = _.contains([1, 2, 3], 2);
+// console.log(res);
+
+var n = require("npm-hello-1");
+
+n.hello();
